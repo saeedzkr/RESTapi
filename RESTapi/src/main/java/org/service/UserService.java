9b68b5d32ac768095953851987bf2b64706dc6a1
saeedzkr <s_zakipour@yahoo.com>
2015-10-19 23:05:23 +0330

    rest api is created by sector 7

---
+++ RESTapi/src/main/java/org/service/UserService.java
@@ -0,0 +1,33 @@
+package org.service;
+
+/**
+ * Created by sector7 on 10/19/15.
+ */
+
+
+import sun.swing.BakedArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/UserService")
+public class UserService {
+
+
+    @GET
+    @Path("/users")
+    @Produces(MediaType.APPLICATION_XML)
+    public List<Integer> getUsers() {
+
+        List<Integer> lst = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            lst.add(i);
+        }
+        return lst;
+    }
+}
